9956739664d9709edf051c08db7c25e97861168f
Andrew Moch <amoch@totalwine.com>
2020-05-13 14:18:03 -0400

    First!

--- src/client/App.js
+++ src/client/App.js
@@ -1,22 +1,111 @@
 import React, { Component } from 'react';
 import './app.css';
-import ReactImage from './react.png';
 
 export default class App extends Component {
-  state = { username: null };
+  state = {
+    iSpindels: [],
+  };
 
   componentDidMount() {
-    fetch('/api/getUsername')
-      .then(res => res.json())
-      .then(user => this.setState({ username: user.username }));
+    const getTheData = () => {
+      fetch('/api/getData')
+        .then((res) => {
+          return res.json();
+        })
+        .then((data) => {
+          this.setState({ iSpindels: data });
+        });
+    };
+
+    const getTheTime = () => {
+      const time = new Date();
+      const formattedNumber = (number) => ('0' + number).slice(-2);
+      this.setState({ currentTime: `${time.getHours()}:${formattedNumber(time.getMinutes())}:${formattedNumber(time.getSeconds())}` });
+    };
+
+    if (window) {
+      window.setInterval(() => getTheData(), 2000);
+      window.setInterval(() => getTheTime(), 1000);
+    }
   }
 
   render() {
-    const { username } = this.state;
+    const { iSpindels, currentTime } = this.state;
+
     return (
       <div>
-        {username ? <h1>{`Hello ${username}`}</h1> : <h1>Loading.. please wait!</h1>}
-        <img src={ReactImage} alt="react" />
+        {iSpindels.length === 0 &&
+        <h1>Waiting for data...</h1>
+        }
+
+        {iSpindels &&
+        iSpindels.length > 0 &&
+        <div>
+          <h1>
+            Current Time: {currentTime}
+          </h1>
+          <div className="cardContainer">
+            {iSpindels.map((iSpindel) => {
+              return (
+                <div className="card">
+                  <h2>
+                    {iSpindel.name} ({iSpindel.ID}) -
+                    <span>{iSpindel.time}</span>
+                  </h2>
+                  <div className="info">
+                    <div className="row">
+                      <div className="left"><strong>Time</strong></div>
+                      <div className="right"><strong>{iSpindel.time}</strong></div>
+                    </div>
+                    <div className="row">
+                      <div className="left">Device Name</div>
+                      <div className="right">{iSpindel.name}</div>
+                    </div>
+                    <div className="row">
+                      <div className="left">Device ID</div>
+                      <div className="right">{iSpindel.ID}</div>
+                    </div>
+                    <div className="row">
+                      <div className="left">Angel</div>
+                      <div className="right">{iSpindel.angle}</div>
+                    </div>
+                    <div className="row">
+                      <div className="left">Temperature</div>
+                      <div className="right">{iSpindel.temperature}</div>
+                    </div>
+                    <div className="row">
+                      <div className="left">Temperature Unit</div>
+                      <div className="right">{iSpindel.temp_units}</div>
+                    </div>
+                    <div className="row">
+                      <div className="left">Battery</div>
+                      <div className="right">{iSpindel.battery}</div>
+                    </div>
+                    <div className="row">
+                      <div className="left">Gravity</div>
+                      <div className="right">{iSpindel.gravity}</div>
+                    </div>
+                    <div className="row">
+                      <div className="left">Interval</div>
+                      <div className="right">{iSpindel.interval}</div>
+                    </div>
+                    <div className="row">
+                      <div className="left">RSSI</div>
+                      <div className="right">{iSpindel.RSSI}</div>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+        }
+
+        <div className="footer">
+          Created by: <a href="https://github.com/themoch/">themoch</a> <br />
+          Fork on Github: <a href="https://github.com/themoch/iSpindel-Logger">https://github.com/themoch/iSpindel-Logger</a> <br />
+          iSpindel Project: <a href="http://www.ispindel.de/">http://www.ispindel.de/</a>
+        </div>
       </div>
     );
   }
--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,38 @@
 const express = require('express');
 const os = require('os');
+const bodyParser = require('body-parser');
+const open = require('open');
 
 const app = express();
+const iSpindelData = [];
 
 app.use(express.static('dist'));
-app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
 
-app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`));
+app.use(bodyParser.json({
+  limit: '100kb',
+}));
+
+app.get('/api/getData', (req, res) => res.send(iSpindelData));
+
+app.use(express.json());
+
+app.post('/', (req, res) => {
+  const existingIndex = iSpindelData.findIndex(spindel => spindel.ID === req.body.ID);
+  const time = new Date();
+  const formattedNumber = ( number ) => ('0' + number).slice(-2);
+  req.body.time = `${time.getHours()}:${formattedNumber(time.getMinutes())}:${formattedNumber(time.getSeconds())}`;
+
+  if (existingIndex > -1) {
+    iSpindelData.splice(existingIndex, 1, req.body);
+  } else {
+    iSpindelData.push(req.body);
+  }
+
+  res.send(iSpindelData);
+});
+
+app.listen(process.env.PORT || 5000, () => {
+  const OSLocal = os.platform() !== 'win32' ? 'http://localhost' : 'http://127.0.0.1';
+  console.log(`Listening on port ${process.env.PORT || 5000}!`);
+  open(`${OSLocal}:${process.env.PORT || 5000}`);
+});
